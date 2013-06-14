cb7ca8b48aaf6bd2d2fdbef6751fc285dc49b63d
Valentin Klinghammer <valentin@quelltextfabrik.de>
2013-06-14 19:09:14 +0200

    Extension now triggers file download dialog correctly

--- contentscript.js
+++ contentscript.js
@@ -1,11 +1,26 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   if(request.action == 'save-image') {
+    // Get all images from page and sort by size
     var images = $('img');
     if(images.length) {
       images.sort(function(img1,img2){
         return img1.width < img2.width ? 1 : -1;
       });
-      window.location = images[0].getAttribute('src');
+
+      // Download the biggest image
+      var file_url = images[0].src;
+      var file_name = images[0].src.substring(images[0].src.lastIndexOf("/") + 1);
+
+      var link = document.createElement('a');
+      link.href = file_url;
+      link.target = '_blank';
+      link.download = file_name || 'unknown';
+
+      var e = document.createEvent('Event');
+      e.initEvent('click', true, true);
+      link.dispatchEvent(e);
+
+      (window.URL || window.webkitURL).revokeObjectURL(link.href);
     }
   }
 });
