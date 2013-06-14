ae5e71929a251bf8c0ba18c2e98ed96a59e7cf2c
Valentin Klinghammer <valentin@quelltextfabrik.de>
2013-06-14 21:52:17 +0200

    Saving of selected images from overlay works

--- README.md
+++ README.md
@@ -8,28 +8,32 @@ This chrome extension should work like this:
 It should allow for a VIM like one-handed, home row operation so you can browse and save pictures while stirring your coffee, petting your cat or whatever, I'm not here to judge.
 
 1. Namely, activate extension via shortcut, e.g. CTRL + SHIFT + S
-2.
-    * If there's only one big image, save it to Download folder
+2. Handle Image Download
+    * If there's only one big image, open file download dialog
     * else display overlay with thumbs of available images, each with size information, press a to save all, 1 for the first, 2 for the second etc.
 3. ???
 4. Profit
 
 #### Initial planed features
 
-* Hotkey to start saving
-* Saving of first or only image to Download folder
-* If more than one big image, show overlay of available images
+##### Done
+* Keyboard shortcut to activate extension
+* Saving of first or only image
+* If more than one image, show overlay of available images
 * Close overlay if Ctrl + Shift + X
-* Save images from overlay
+* Save selected images from overlay
+
+##### Todo
+* Save all images from overlay
 * Refine which image sizes should be downloaded
 * Close overlay if click on blank area
 
 #### Future possible features
 
-* Have an option to switch to right handed layout
-* Have an option to customize keys
 * Have an option to save on https, too
+* Have an option to customize keys
 * Have an option to black list urls
 * Have option to either white list url access
 * Let user customize minimum image sizes for saving (pixel size and disk space)
-* Work with thumbs on page and save full sized images
+* Follow link on thumbs on page and save full sized images
+    This means, if link target is image, save, if not image, save biggest image
--- contentscript.js
+++ contentscript.js
@@ -1,6 +1,13 @@
+// Funtion returns jquery object with many jquery objects
+function get_images() {
+  return $('img');
+}
+
 // Function takes a jquery object as an argument
 // and triggers the file download dialogue
-function download_image(image) {
+function download_image(requested) {
+  var images = get_images();
+  var image = images[requested];
   var file_url = image.src;
   var file_name = image.src.substring(image.src.lastIndexOf("/") + 1);
 
@@ -16,47 +23,76 @@ function download_image(image) {
   (window.URL || window.webkitURL).revokeObjectURL(link.href);
 }
 
+function keypress_handler(e) {
+  if((e.which > 48) && (e.which < 58)) {
+    // Substract 48 to get number pressed, ie 49-48=1, 57-48=9
+    // Substract  1 to get array position, your know, 'cause arrays start at 0
+    var requested = e.which - 48 - 1;
+    download_image(requested);
+  }
+  e.preventDefault();
+}
+
+// TODO remove on page refresh, too
+function add_keypress_listener(images) {
+  $('body').on('keydown', {arg1: images}, keypress_handler);
+}
+
+function remove_keypress_listener() {
+  $('body').off('keydown');
+}
+
 // Function takes a jquery object with many jquery objects
 // opens an overlay to the current page
 function show_overlay(images) {
   var overlay = $('#image-overlay');
 
-  if(overlay.length) {
+  // if(overlay.length) {
+  // This doesn't work well when visiting a new page in the same tab, commented out for now
+  if(false) {
     // If we have just hidden the overly, simply make visible again
     overlay.show();
   } else {
     overlay = jQuery('<div id="image-overlay"></div>');
     overlay.appendTo(document.body);
     overlay = $('#image-overlay');
+
+    overlay.append(
+      '<h1>One-Handed Image Downloader</h1>'
+      + '<p>Hit A to save all images, 1, 2, 3 etc. to save the numbered images</p>'
+      + '<p>Hit Ctrl + Shift + X to close this overlay</p>'
+    );
+
     overlay.append('<ul></ul>');
     var list = $('#image-overlay ul');
 
-    list.append('<li><img src="http://placehold.it/300x300&text=Download+All"></li>');
-    images.each(function(){
-      list.append('<li><img src="' + $(this).attr('src') + '"></li>');
+    // list.append('<li><img src="http://placehold.it/300x300&text=Download+All"></li>');
+    images.each(function(index){
+      list.append('<li><img src="' + $(this).attr('src') + '"><span>' + (index+1) + '</span></li>');
     });
+    add_keypress_listener();
   }
 }
 
 function save_image() {
   // Get all images from page
-  var images = $('img');
-    if(images.length > 1) {
-      // If there is more than one image, sort by size and let user choose
-      images.sort(function(img1,img2){
-        return img1.width < img2.width ? 1 : -1;
-      });
-      show_overlay(images);
-    } else if(images.length == 1) {
-      // Download the biggest image if there's only one
-      download_image(images[0]);
-    } else {
-      // No suitable image found, TODO handle that!
-    }
+  var images = get_images();
+  if(images.length > 1) {
+    // Show overlay for multiple images
+    show_overlay(images);
+  } else if(images.length == 1) {
+    // Download the biggest image if there's only one
+    download_image(images[0]);
+  } else {
+    // No suitable image found, TODO handle that!
+  }
 }
 
 function close_overlay() {
-  $('#image-overlay').hide();
+  // $('#image-overlay').hide();
+  // Hiding is not enough if visiting pages in same tab
+  $('#image-overlay').remove();
+  remove_keypress_listener();
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
